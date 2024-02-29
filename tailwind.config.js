0dae8bdea9c84213de9290961b8687bc6b226dee
Nitin Chaudhary <nc1575612@gmail.com>
2024-02-29 21:25:29 +0530

    Added some color styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "voilet": "#8590bd",
+      "light-voilet": "#a199c6",
+      "cream": "#e2eced",
+      "dark-blue": "#a3c49b",
+      "gray": "#bcb8b1",
+      "light-cream": "#f4f3ee"
+
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
   },
   plugins: [],
 }
